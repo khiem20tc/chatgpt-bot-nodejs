8fbf509e70d1f9532cc3e807cf48e345322fb71a
khiem20tc <khiem20tc@gmail.com>
2023-03-16 16:40:58 +0700

    fix(promt): edit promt for flexible

--- services/chatgpt.service.js
+++ services/chatgpt.service.js
@@ -2,7 +2,7 @@ const {Configuration, OpenAIApi} = require("openai");
 const DbService = require('./db.service');
 
 class ChatGPTService {
-  rolePlayIntroduction = 'Trong vai một chatbot chuyên gia với tên là Bot ngáo, nhiệm vụ chính của bạn là tâm sự với người dùng như một người am hiểu về cuộc sống, xã hội, các sự kiện chính trị, các vấn đề thời sự cũng như sẵn sàng đáp ứng mọi yêu cầu của người dùng, sẵn sàng lắng nghe và đề cao vai trò và cảm xúc của người dùng khi trò chuyện. Bạn phải ghi nhớ cả các thông tin mà người dùng đã đưa ra trong cuộc trò chuyện. Trong khi trò chuyện, các dẫn chứng và ví dụ cần được đưa ra nhằm hỗ trợ cho các lập luận lẫn đề xuất mà bạn đưa ra. Lưu ý phải luôn giữ cuộc trò chuyện vui vẻ và thoải mái.';
+  rolePlayIntroduction = '';
   async generateCompletion(prompt, user) {
     // Lấy đống tin nhắn cũ ra
     const oldMessages = await DbService.getUserMessages(user._id);
@@ -19,13 +19,13 @@ class ChatGPTService {
       fullPrompt += 'CHAT:\n';
       // nếu có tin nhắn cũ thì thêm đoạn tin nhắn cũ đấy vào nội dung chat
       for (let message of oldMessages) {
-        fullPrompt += `Người dùng: ${message.userMessage}\n`;
-        fullPrompt += `Bot ngáo: ${message.botMessage}\n\n`;
+        fullPrompt += `User: ${message.userMessage}\n`;
+        fullPrompt += `Bot: ${message.botMessage}\n\n`;
       }
     }
 
-    fullPrompt += `Người dùng: ${prompt}\n`;
-    fullPrompt += `Bot ngáo: `;
+    fullPrompt += `User: ${prompt}\n`;
+    fullPrompt += `Bot: `;
 
     console.log(fullPrompt);
 
@@ -34,7 +34,7 @@ class ChatGPTService {
       model: "text-davinci-003",
       prompt: fullPrompt,
       temperature: 0.7,
-      max_tokens: 1000,
+      max_tokens: 2000,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
